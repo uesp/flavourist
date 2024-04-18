4d024efcd24a063b38c6f4e68333a0814add1708
TJ <irgmisg4nxc9@beconfidential.com>
2024-04-18 17:48:33 +0100

    delete unnecessary test cases

--- test_resources/ios/ios_firebase_script_processor_test/multiple_flavors.sh
+++
@@ -1,7 +0,0 @@
-if [ "$CONFIGURATION" == "Debug-apple" ] || [ "$CONFIGURATION" == "Release-apple" ]; then
-  cp Runner/apple/GoogleService-Info.plist Runner/GoogleService-Info.plist
-elif [ "$CONFIGURATION" == "Debug-banana" ] || [ "$CONFIGURATION" == "Release-banana" ]; then
-  cp Runner/banana/GoogleService-Info.plist Runner/GoogleService-Info.plist
-elif [ "$CONFIGURATION" == "Debug-pear" ] || [ "$CONFIGURATION" == "Release-pear" ]; then
-  cp Runner/pear/GoogleService-Info.plist Runner/GoogleService-Info.plist
-fi
--- test_resources/ios/ios_firebase_script_processor_test/single_flavor.sh
+++
@@ -1,3 +0,0 @@
-if [ "$CONFIGURATION" == "Debug-apple" ] || [ "$CONFIGURATION" == "Release-apple" ]; then
-  cp Runner/apple/GoogleService-Info.plist Runner/GoogleService-Info.plist
-fi
